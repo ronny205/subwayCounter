bc87ef0d42a06b3e1f90c904552d465c371e4b86
ronny205 <rohanaiwale99@gmail.com>
2023-07-12 20:08:27 +0530

    changed index.js file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
+
+console.log(saveEl);
+
 document.getElementById("count-el").innerText = count;
 function increment() {
   count = count + 1;
   countEl.innerText = count;
 }
+
 function save() {
-  console.log(count);
+  let countStr = `${count} - `;
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0
 }
